f5c31a82b2f7ba83ae3408a9959a34a15c2794b8
Sophia Alexandra Gould <gould7@ecegrid-thin1.ecn.purdue.edu>
2017-02-03 20:09:37 -0500

    updated

--- answer04.c
+++ answer04.c
@@ -7,10 +7,14 @@
 #ifdef TEST_INTEGRATE
 void integrate(Integration * intrg)
 {
+  intrg -> answer = 0;
+  double reg = 0;
+  reg = (intrg -> upperlimit - intrg -> lowerlimit) / (intrg -> intervals);
+  for (int i = 0; i < intrg -> intervals; i++){
+  	intrg -> answer += reg * intrg -> func(intrg -> lowerlimit + i * reg);
+  }
   // integrate the function stored in intrg's func
   // store the result in intrg's answer
-
-
 
 }
  #endif // TEST_INTEGRATE
@@ -25,25 +29,25 @@ bool  runIntegrate(char * infilename, char * outfilename)
 // return false when encountering any problem
 {
   // open the input file name for reading
+	FILE * inf = fopen(infilename, "r");
+	if (inf == NULL){
+		return false;
+	}
 
-
-
+	Integration intrg;
   // if fopen fails, return false
-
-
-
   // read one double from the input file and store it in 
   // intrg's lowerlimit
   // use fscanf
   // check the return value. If the return value is not one
   // close the file and return false
 
-
-
-
-
-
-
+	int n;
+	n = fscanf(inf, "%lf\n", &intrg.lowerlimit);
+	if (n != 1){
+		fclose(inf);
+		return false;
+	}
 
   // read one double from the input file and store it in 
   // intrg's upperlimit
@@ -51,15 +55,11 @@ bool  runIntegrate(char * infilename, char * outfilename)
   // check the return value. If the return value is not one
   // close the file and return false
 
-
-
-
-
-
-
-
-
-
+	n = fscanf(inf, "%lf", &intrg.upperlimit);
+	if (n != 1){
+		fclose(inf);
+		return false;
+	}
 
   // read one int from the input file and store it in 
   // intrg's intervals
@@ -67,71 +67,59 @@ bool  runIntegrate(char * infilename, char * outfilename)
   // check the return value. If the return value is not one
   // close the file and return false
 
-
-
-
-
-
-
+	n = fscanf(inf,"%d", &intrg.intervals);
+	if (n != 1){
+		fclose(inf);
+		return false;
+	}
 
   // close the input file
 
-
-
-
-
-
+	fclose(inf);
 
   // open the output file for writing
   // if fopen fails, return false
 
-
-
-
-
-
-
+	inf = fopen(outfilename, "w");
+	if (inf == NULL){
+		return false;
+	}
 
   // create an array of funcptr called funcs with five elements:
   // func1, func2, ..., func5
 
-
-
+	funcptr funcs[5] = {func1, func2, func3, func4, func5};
 
   // go through the elements in funcs 
   // for each element, call integrate for that function
   // write the result (stored in intrg's answer to 
   // the output file. each answer occupies one line (add "\n")
   // use fprintf
-
-
-
+	
+    for(int y = 0; y < 5; y++){
+		intrg.func = funcs[y];
+		integrate(&intrg);
+		n = fprintf(inf, "%lf %s", intrg.answer, "\n");
+	}
 
 
   // check the return value of fprintf. 
   // If it is less one one, close the output
   // file and return false
 
-
-
-
-
+	if(n < 1){
+		fclose(inf);
+		return false;
+	}
 
 
   // after going through all functions in funcs
   // close the output file
-
-
-
-
-
-
+	fclose(inf);
 
   // if the function reaches here, return true
 
-
-
-
+	return true;
 
 }
 #endif // RUN_INTEGRATE
